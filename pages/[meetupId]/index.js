cb2c669b48e17d15c3bdbbbe3ffe8a8c50f397ed
Daphne <daphneruth304@gmail.com>
2022-07-27 12:12:15 +0300

    updated

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -16,7 +16,14 @@ function MeetupDetails() {
 export async function getStaticProps(){
   return {
     props:{
-      
+      meetupData: {
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+        id: meetupId,
+        title: 'First Meetup',
+        address: 'Some Street 5, Some City',
+        description: 'This is a first meetup',
+      },
     }
   }
 }
